784e0203e677c0aa723bc471581591137dcead33
Tiago Teixeira <108500810+TiagovskiCode@users.noreply.github.com>
2026-08-11 12:35:03 +0900

    Initial commit

---
+++ 02_formatingcode.py
@@ -0,0 +1,5 @@
+# In Python between every word you need to have a space
+
+x=1 # This is not correct by the Python community
+
+x = 1 # Now writing like this is correct and it is according to PEP8
---
+++ 03_runningcode.py
@@ -0,0 +1,3 @@
+# In PyCharm to run code you need to press Shift + F10
+
+print("Hello world 😁")
