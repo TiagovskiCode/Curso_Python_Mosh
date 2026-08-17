c2a8cb52a14e42355b5c7734cc6a1dcadd14e72d
Tiago Teixeira <108500810+TiagovskiCode@users.noreply.github.com>
2026-08-17 20:37:55 +0900

    more learning

---
+++ Classes/21_defining_functions.py
@@ -0,0 +1,9 @@
+# A function is a reusable block of code that performs a specific task.
+# Use 'def' to define a function and call it by its name followed by parentheses.
+
+def greet():
+    print("Hi there")
+    print("Welcome aboard")
+
+
+greet()
